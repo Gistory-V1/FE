d17520c9023c58b468717cf632ea5b44e4a3f813
bae080311 <s24024@gsm.hs.kr>
2024-12-17 20:46:12 +0900

    Feat: color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,15 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        gray1: "a6a6a6",
+        gray2: "d8d8d8",
+        gray3: "757575",
+        gray4: "eeeeee",
+        black: "111111",
+      },
+    },
   },
   plugins: [],
 };
