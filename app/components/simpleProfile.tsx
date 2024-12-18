fc6285e80b90cf5b103bf5d9efbd00046f0a438d
bae080311 <s24024@gsm.hs.kr>
2024-12-18 20:47:15 +0900

    Feat: simple profile style

--- app/components/simpleProfile.tsx
+++ app/components/simpleProfile.tsx
@@ -3,10 +3,15 @@ import Profile from "../svg/logo.svg";
 
 export default function SimpleProfile() {
   return (
-    <div className="flex flex-col items-center justify-center border border-gray1">
-      <Image src={Profile} alt="프로필" />
-      <h3>모태환 님의 블로그</h3>
-      <span>모태환 님의 블로그 입니다</span>
+    <div
+      style={{ border: "1px solid #A6A6A6" }}
+      className="flex flex-col gap-6 items-center justify-center px-10 py-4"
+    >
+      <Image width={100} height={100} src={Profile} alt="프로필" />
+      <div className="flex flex-col items-center gap-2">
+        <h3 className="font-bold">모태환 님의 블로그</h3>
+        <span className="text-[12px]">모태환 님의 블로그 입니다</span>
+      </div>
     </div>
   );
 }
