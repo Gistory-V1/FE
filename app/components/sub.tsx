c0faf61892d7727a1000cf8e2aab499bb31fde2f
bae080311 <s24024@gsm.hs.kr>
2024-12-18 10:01:18 +0900

    Feat: 구독 컴포넌트 폼

---
+++ app/components/sub.tsx
@@ -0,0 +1,11 @@
+export default function Sub() {
+  return (
+    <div>
+      <div>
+        <h3>{}</h3>
+        <span>✅ 구독자 {}명</span>
+      </div>
+      <button>+ 구독</button>
+    </div>
+  );
+}
