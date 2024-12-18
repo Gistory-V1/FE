a5b2cd542bcf4d848b1fbe52e95f7ff4897ea8b1
bae080311 <s24024@gsm.hs.kr>
2024-12-18 18:17:20 +0900

    Feat: pop up

--- app/components/postOptions.tsx
+++ app/components/postOptions.tsx
@@ -3,16 +3,28 @@
 import Image from "next/image";
 import Heart from "../svg/heart.svg";
 import fill from "../svg/fillHeart.svg";
+import Option from "../svg/option.svg";
 import { useState } from "react";
 
 export default function PostOptions() {
   const [isLiked, setIsLiked] = useState(false);
+  const [option, setOption] = useState(false);
   return (
-    <div>
-      {!isLiked ? (
-        <Image src={Heart} alt="빈 하트" onClick={() => setIsLiked(true)} />
-      ) : (
-        <Image src={fill} alt="하트" onClick={() => setIsLiked(false)} />
+    <div className="flex justify-between items-center mt-4">
+      <div className="flex items-center">
+        {!isLiked ? (
+          <Image src={Heart} alt="빈 하트" onClick={() => setIsLiked(true)} />
+        ) : (
+          <Image src={fill} alt="하트" onClick={() => setIsLiked(false)} />
+        )}
+        <span>22</span>
+      </div>
+      <Image src={Option} alt="옵션" onClick={() => setOption(!option)} />
+      {option && (
+        <ul>
+          <li style={{ borderBottom: "1px solid #A6A6A6" }}>수정</li>
+          <li>삭제</li>
+        </ul>
       )}
     </div>
   );
