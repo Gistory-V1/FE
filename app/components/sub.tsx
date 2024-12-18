82ccd7b4d80899ae43fe3d4dc1eab26e3b1cf3ec
bae080311 <s24024@gsm.hs.kr>
2024-12-18 22:16:23 +0900

    Feat: 구독 function

--- app/components/sub.tsx
+++ app/components/sub.tsx
@@ -1,14 +1,24 @@
 import Image from "next/image";
 import Plus from "../svg/plus.svg";
+import axios from "axios";
+import { url } from "../../config";
 
 export default function Sub() {
+  function subscribe() {
+    axios.post(`${url}/sub`).catch((err) => {
+      console.log(err);
+    });
+  }
   return (
     <div className="flex justify-between items-center w-[420px] rounded-md p-4 bg-gray4">
       <div className="flex  gap-2 flex-col">
         <h3 className="text-20 font-bold">{"배경진"}</h3>
         <span className="text-gray3 text-[12px]">✅ 구독자 {3}명</span>
       </div>
-      <button className="gap-1 border border-black1 text-[14px] py-2 px-3 rounded-2xl flex items-center justify-center">
+      <button
+        onClick={subscribe}
+        className="gap-1 border border-black1 text-[14px] py-2 px-3 rounded-2xl flex items-center justify-center"
+      >
         <Image src={Plus} alt="구독" />
         구독
       </button>
