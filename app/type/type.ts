3dce3e4ccbe805df369c1a02f6e0bd2e7b3427ba
bae080311 <s24024@gsm.hs.kr>
2024-12-18 10:00:23 +0900

    Feat: post type

---
+++ app/type/type.ts
@@ -0,0 +1,11 @@
+interface PostProps {
+  postId: number;
+  title: string;
+  author: string;
+  view: number;
+  createdAt: string;
+  sub: number;
+  like: number;
+}
+
+export default PostProps;
