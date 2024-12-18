3f5fac473647b1a3fcb9b58c78e23689c74db341
bae080311 <s24024@gsm.hs.kr>
2024-12-18 20:00:57 +0900

    Remove: button

--- app/components/simpleProfile.tsx
+++ app/components/simpleProfile.tsx
@@ -3,11 +3,10 @@ import Profile from "../svg/logo.svg";
 
 export default function SimpleProfile() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center border border-gray1">
       <Image src={Profile} alt="프로필" />
       <h3>모태환 님의 블로그</h3>
       <span>모태환 님의 블로그 입니다</span>
-      <button>글쓰기</button>
     </div>
   );
 }
