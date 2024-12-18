b4fcac2b4f61d1b36399436f30e4b64aedea5087
bae080311 <s24024@gsm.hs.kr>
2024-12-18 20:48:17 +0900

    Fix: pop up location

--- app/components/postOptions.tsx
+++ app/components/postOptions.tsx
@@ -10,21 +10,23 @@ export default function PostOptions() {
   const [isLiked, setIsLiked] = useState(false);
   const [option, setOption] = useState(false);
   return (
-    <div
-      style={{ border: "1px solid #A6A6A6" }}
-      className="flex justify-between items-center rounded-2xl px-2 mt-4 py-1 w-[100px]"
-    >
-      <div className="flex items-center">
-        {!isLiked ? (
-          <Image src={Heart} alt="빈 하트" onClick={() => setIsLiked(true)} />
-        ) : (
-          <Image src={fill} alt="하트" onClick={() => setIsLiked(false)} />
-        )}
-        <span>22</span>
+    <div>
+      <div
+        style={{ border: "1px solid #A6A6A6" }}
+        className="flex justify-between items-center rounded-2xl px-2 mt-4 py-1 w-[100px]"
+      >
+        <div className="flex items-center">
+          {!isLiked ? (
+            <Image src={Heart} alt="빈 하트" onClick={() => setIsLiked(true)} />
+          ) : (
+            <Image src={fill} alt="하트" onClick={() => setIsLiked(false)} />
+          )}
+          <span>22</span>
+        </div>
+        <Image src={Option} alt="옵션" onClick={() => setOption(!option)} />
       </div>
-      <Image src={Option} alt="옵션" onClick={() => setOption(!option)} />
       {option && (
-        <ul>
+        <ul className="">
           <li style={{ borderBottom: "1px solid #A6A6A6" }}>수정</li>
           <li>삭제</li>
         </ul>
