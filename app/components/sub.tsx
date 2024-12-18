6dcd1e374bf3c4ede505b642087ef0342accfc0b
bae080311 <s24024@gsm.hs.kr>
2024-12-18 11:57:36 +0900

    Feat: 구독 컴포넌트트

--- app/components/sub.tsx
+++ app/components/sub.tsx
@@ -1,11 +1,17 @@
+import Image from "next/image";
+import Plus from "../svg/plus.svg";
+
 export default function Sub() {
   return (
-    <div>
-      <div>
-        <h3>{}</h3>
-        <span>✅ 구독자 {}명</span>
+    <div className="flex justify-between items-center w-[420px] rounded-md p-4 bg-gray4">
+      <div className="flex  gap-2 flex-col">
+        <h3 className="text-20 font-bold">{"배경진"}</h3>
+        <span className="text-gray3 text-[12px]">✅ 구독자 {3}명</span>
       </div>
-      <button>+ 구독</button>
+      <button className="gap-1 border border-black1 text-[14px] py-2 px-3 rounded-2xl flex items-center justify-center">
+        <Image src={Plus} alt="구독" />
+        구독
+      </button>
     </div>
   );
 }
