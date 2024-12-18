544ea5abe421a70aaf9d65f375f2646b9dc5240a
bae080311 <s24024@gsm.hs.kr>
2024-12-18 14:40:40 +0900

    Fix: profile style

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -1,21 +1,35 @@
+import Image from "next/image";
+import ProfileImage from "../svg/logo.svg";
+
 export default function Profile() {
   return (
     <div className="flex flex-col items-start p-4">
-      <h3 className="text-lg font-bold mb-2">모태환 님의 블로그</h3>
-      <div className="mb-4">
-        <span className="text-gray-600">구독자 </span>
-        <span className="font-semibold">0명</span>
+      <div className="flex gap-2 items-center justify-center mb-3">
+        <Image width={50} height={50} src={ProfileImage} alt="프로필" />
+        <div>
+          <h3 className="text-lg font-bold mb-1 mt-4">모태환 님의 블로그</h3>
+          <div className="mb-4">
+            <span className="text-gray-600">구독자 </span>
+            <span className="font-semibold">0명</span>
+          </div>
+        </div>
       </div>
-      <div className="border-solid flex items-center borders rounded-lg px-6 py-3">
+      <div
+        style={{ border: "1px solid" }}
+        className="border-solid gap-4 flex  items-center rounded-lg px-8 py-4"
+      >
         <span className="font-bold cursor-pointer">글쓰기</span>
         <div className="h-5 mx-6 text-gray-400">|</div>
         <span className="font-bold cursor-pointer">내 블로그</span>
         <div className="h-5 mx-6 text-gray-400">|</div>
-        <span className="font-bold cursor-pointer">관리</span>
+        <span className="font-bold cursor-pointer"> 관리 </span>
       </div>
-      <div>
-        <span>조회수</span>
-        <span>0회</span>
+      <div
+        style={{ borderBottom: "1px solid #111111" }}
+        className=" flex my-8 justify-between w-full"
+      >
+        <span className="">조회수</span>
+        <span className="mb-8">0회</span>
       </div>
     </div>
   );
