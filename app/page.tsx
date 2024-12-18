2dc0f0d4e0c69b5784b355acaa2a177da5659cbc
bae080311 <s24024@gsm.hs.kr>
2024-12-18 09:28:55 +0900

    Feat: layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import "../app/globals.css";
+import Header from "./components/header";
 
 export default function Home() {
   return (
     <div>
-      <input type="text" className="bg-black" />
+      <Header />
+      <div className="flex w-2/3">
+        <div></div>
+        <div className="flex columns border-l border-l-gray-300">
+          <h4>gistory에 로그인하여 블로그를 즐겨보아요</h4>
+          <button></button>
+        </div>
+      </div>
     </div>
   );
 }
