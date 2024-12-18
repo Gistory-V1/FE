487398145820eb31ea39c91e0a2e6cde1a43b268
bae080311 <s24024@gsm.hs.kr>
2024-12-18 22:20:16 +0900

    Feat: sub cancel function

--- app/components/sub.tsx
+++ app/components/sub.tsx
@@ -1,13 +1,30 @@
+"use client";
+
 import Image from "next/image";
 import Plus from "../svg/plus.svg";
 import axios from "axios";
 import { url } from "../../config";
+import { useState } from "react";
 
 export default function Sub() {
+  const [count, setCount] = useState(0);
+  const [sub, setSub] = useState(false);
   function subscribe() {
     axios.post(`${url}/sub`).catch((err) => {
       console.log(err);
     });
+  }
+  function cancel() {
+    setSub(false);
+    axios
+      .delete(`${url}/sub`)
+      .then((res) => {
+        setCount(res.data.subCount);
+      })
+      .catch((err) => {
+        console.log(err);
+        setCount(count + 1);
+      });
   }
   return (
     <div className="flex justify-between items-center w-[420px] rounded-md p-4 bg-gray4">
@@ -16,7 +33,7 @@ export default function Sub() {
         <span className="text-gray3 text-[12px]">✅ 구독자 {3}명</span>
       </div>
       <button
-        onClick={subscribe}
+        onClick={sub ? cancel : subscribe}
         className="gap-1 border border-black1 text-[14px] py-2 px-3 rounded-2xl flex items-center justify-center"
       >
         <Image src={Plus} alt="구독" />
