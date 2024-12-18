ca14332d1aeea64e3513dc5bc84051764a10ef00
bae080311 <s24024@gsm.hs.kr>
2024-12-18 20:02:39 +0900

    Feat: pop up form

---
+++ app/components/popUp.tsx
@@ -0,0 +1,15 @@
+export default function PopUp() {
+  return (
+    <div>
+      <div>
+        <h4>모태환</h4>
+        <span>s24023@gsm.hs.kr</span>
+      </div>
+      <ul className="flex gap-2" style={{ borderTop: "1px solid #A6A6A6" }}>
+        <li>로그아웃</li>
+        <li>|</li>
+        <li>회원탈퇴</li>
+      </ul>
+    </div>
+  );
+}
