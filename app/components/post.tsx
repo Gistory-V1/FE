c7c50ed84a1d8a7a001f09a9a15e7546fdde4add
bae080311 <s24024@gsm.hs.kr>
2024-12-18 12:15:15 +0900

    Feat: post component

--- app/components/post.tsx
+++ app/components/post.tsx
@@ -4,15 +4,15 @@ import PostProps from "../type/type";
 
 export default function Post({ post }: { post: PostProps }) {
   return (
-    <div>
-      <h3>{post.title}</h3>
-      <ul>
-        <li>
+    <div className="flex flex-col gap-5 px-4 py-3 border-b border-b-gray-400 w-[420px]">
+      <h3 className="font-bold">{post.title}</h3>
+      <ul className="flex gap-4 items-center text-gray1 text-[12px]">
+        <li className="flex gap-1 items-center">
           <Image src={Heart} alt="좋아요" />
           <span>{post.like}</span>
         </li>
         <li>{post.view} 조회</li>
-        <li>{post.createdAt}일 전</li>
+        <li>{post.createdAt}</li>
       </ul>
     </div>
   );
