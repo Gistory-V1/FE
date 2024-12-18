99d68ab932d061354442f6be534d486d8bd363b8
bae080311 <s24024@gsm.hs.kr>
2024-12-18 12:28:18 +0900

    Feat: lank component

---
+++ app/components/lank.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Heart from "../svg/heart.svg";
+import PostProps from "../type/type";
+
+export default function Lank({ post }: { post: PostProps }) {
+  return (
+    <div className="flex flex-col gap-5 px-4 py-3 border-b border-b-gray-400 w-[420px]">
+      <div className="text-gray3">
+        <span className="font-bold text-[30px]">1/</span>
+        <span>{post.author}</span>
+      </div>
+      <h3 className="font-bold">{post.title}</h3>
+      <ul className="flex gap-4 items-center text-gray1 text-[12px]">
+        <li className="flex gap-1 items-center">
+          <Image src={Heart} alt="좋아요" />
+          <span>{post.like}</span>
+        </li>
+        <li>{post.view} 조회</li>
+        <li>{post.createdAt}</li>
+      </ul>
+    </div>
+  );
+}
