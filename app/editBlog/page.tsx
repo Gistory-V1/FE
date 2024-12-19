dee97155c661fa7e5e6ae5963f739608d9d6d2b6
bae080311 <s24024@gsm.hs.kr>
2024-12-19 15:40:16 +0900

    Feat: localstorage logic

--- app/editBlog/page.tsx
+++ app/editBlog/page.tsx
@@ -3,31 +3,54 @@
 import { useEffect, useState } from "react";
 import Header from "../components/header";
 import Black from "../components/black";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import axios from "axios";
 import { url } from "../../config";
 
 export default function EditPage() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [id, setId] = useState<string | null>(null);
   const router = useRouter();
-  const { id } = router.query;
 
-  if (!id) {
-    return <div>Loading...</div>;
-  }
   useEffect(() => {
-    axios.get(`${url}/post/${id}`).then((res) => {
-      setTitle(res.data.title);
-      setContent(res.data.content);
-    });
+    const storedId = localStorage.getItem("postId");
+    if (storedId) {
+      setId(storedId);
+      localStorage.removeItem("postId");
+    }
   }, []);
+
+  useEffect(() => {
+    if (id) {
+      axios
+        .get(`${url}/post/${id}`)
+        .then((res) => {
+          setTitle(res.data.title);
+          setContent(res.data.content);
+        })
+        .catch((err) => {
+          console.error("데이터 로딩 실패", err);
+        });
+    }
+  }, [id]);
+
   function send() {
-    axios.put(`${url}/edit`, {
-      postId: id,
-      title: title,
-      content: content,
-    });
+    if (id) {
+      axios
+        .put(`${url}/edit`, {
+          postId: Number(id),
+          title: title,
+          content: content,
+        })
+        .then((res) => {
+          console.log("수정되었습니다", res);
+          router.push(`/post/${id}`);
+        })
+        .catch((err) => {
+          console.error("수정 실패", err);
+        });
+    }
   }
 
   return (
@@ -35,14 +58,16 @@ export default function EditPage() {
       <Header />
       <div className="flex flex-col items-center justify-center">
         <input
+          value={title}
           onChange={(e) => setTitle(e.target.value)}
           type="text"
           className="border-b w-2/3 text-[32px] py-8"
           placeholder="제목을 입력하세요"
         />
         <textarea
+          value={content}
           onChange={(e) => setContent(e.target.value)}
-          className="py-6 focus:outline-none border-none h-[400px] w-2/3  border rounded resize-none overflow-hidden"
+          className="py-6 focus:outline-none border-none h-[400px] w-2/3 border rounded resize-none overflow-hidden"
           placeholder="여러분들의 생각을 적어보세요"
         />
         <Black label="작성" onClick={send} />
