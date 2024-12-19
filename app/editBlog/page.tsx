5e645868081b17dcd5074ffb5b9fe33958758ac1
bae080311 <s24024@gsm.hs.kr>
2024-12-19 15:03:04 +0900

    Feat: get title, content

--- app/editBlog/page.tsx
+++ app/editBlog/page.tsx
@@ -1,29 +1,43 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Header from "../components/header";
 import Black from "../components/black";
-import { useRef } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 export default function EditPage() {
-  const title = useRef<HTMLInputElement>(null);
-  const content = useRef<HTMLTextAreaElement>(null);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const router = useRouter();
+  const { id } = router.query;
+
+  if (!id) {
+    return <div>Loading...</div>;
+  }
+  useEffect(() => {
+    axios.get(`/post/${id}`).then((res) => {
+      setTitle(res.data.title);
+      setContent(res.data.content);
+    });
+  }, []);
+
   return (
     <div>
       <Header />
       <div className="flex flex-col items-center justify-center">
         <input
-          ref={title}
+          onChange={(e) => setTitle(e.target.value)}
           type="text"
           className="border-b w-2/3 text-[32px] py-8"
           placeholder="제목을 입력하세요"
         />
         <textarea
-          ref={content}
+          onChange={(e) => setContent(e.target.value)}
           className="py-6 focus:outline-none border-none h-[400px] w-2/3  border rounded resize-none overflow-hidden"
           placeholder="여러분들의 생각을 적어보세요"
         />
-
-        <Black label="작성" />
+        <Black label="작성" onClick={} />
       </div>
     </div>
   );
