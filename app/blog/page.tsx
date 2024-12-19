6b7c27137de8f29572d409015f8313ea93a4cdd8
bae080311 <s24024@gsm.hs.kr>
2024-12-19 10:26:17 +0900

    Feat: blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,23 +1,37 @@
-import Button from "../components/button";
 import Header from "../components/header";
+import Post from "../components/post";
 import SimpleProfile from "../components/simpleProfile";
 
 export default function BlogPage() {
   return (
     <div>
       <Header />
-      <div className="w-full flex justify-center p-4">
-        <div className="w-4/5">
-          <div className="w-2/3">
-            <h1>모태환 님의 블로그</h1>
-            <h3>전체 글</h3>
+      <div className=" w-full flex justify-center p-4">
+        <div className="w-4/5 flex">
+          <div className="w-2/3 flex flex-col gap-8">
+            <h1 className="font-bold text-[26px]">모태환 님의 블로그</h1>
+            <div
+              style={{ borderBottom: "1px solid #A6A6A6" }}
+              className="flex font-bold text-[20px] pb-6 w-[650px]"
+            >
+              <h3>전체 글 </h3>
+              <h3 className="text-red-600"> 1</h3>
+            </div>
+            <Post post={example} />
           </div>
-          <div className="flex">
-            <SimpleProfile />
-            <button>구독하기</button>
-          </div>
+          <SimpleProfile />
         </div>
       </div>
     </div>
   );
 }
+
+const example = {
+  postId: 1,
+  title: "dps",
+  author: "skdi",
+  view: 2,
+  createdAt: "23시간 전",
+  sub: 3,
+  like: 3,
+};
