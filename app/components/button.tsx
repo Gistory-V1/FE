6e1e24376cf0777a7e366d165096e8f65e259c2f
bae080311 <s24024@gsm.hs.kr>
2024-12-27 19:56:05 +0900

    function optional

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 interface ButtonProps {
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 export default function Button({ label, onClick }: ButtonProps) {
