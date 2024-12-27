82954704adc4ebe212ff2774f56e460042d1d7cb
bae080311 <s24024@gsm.hs.kr>
2024-12-27 19:26:45 +0900

    Fix: margin

--- app/components/loginOption.tsx
+++ app/components/loginOption.tsx
@@ -2,7 +2,7 @@ import Button from "../components/button";
 
 export default function LoginOption() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col my-8">
       <h4 className="text-xs text-left">
         gistory에 로그인하여 블로그를 즐겨보아요
       </h4>
