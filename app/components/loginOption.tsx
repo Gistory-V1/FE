7835eaef0ad7d7cae423b8ccbe9ee512fe3ef014
bae080311 <s24024@gsm.hs.kr>
2024-12-27 19:38:51 +0900

    feat: 경로로

--- app/components/loginOption.tsx
+++ app/components/loginOption.tsx
@@ -1,13 +1,15 @@
+import { useRouter } from "next/navigation";
 import Button from "../components/button";
 
 export default function LoginOption() {
+  const route = useRouter();
   return (
     <div className="flex flex-col my-8">
       <h4 className="text-xs text-left">
         gistory에 로그인하여 블로그를 즐겨보아요
       </h4>
-      <Button label="로그인" />
-      <Button label="회원가입" />
+      <Button onClick={() => route.push("/login")} label="로그인" />
+      <Button onClick={() => route.push("/sign-up")} label="회원가입" />
     </div>
   );
 }
