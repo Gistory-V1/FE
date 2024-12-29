4680561beb275a51e25b1d66c4886f30a5c63964
bae080311 <s24024@gsm.hs.kr>
2024-12-30 01:13:10 +0900

    Feat: cursor pointer

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,16 +2,27 @@ import Image from "next/image";
 import TextLogo from "../svg/textlogo.svg";
 import Logo from "../svg/logo.svg";
 import Search from "../svg/search.svg";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
+  const router = useRouter();
   return (
     <header
       style={{ borderBottom: "1px solid #868686" }}
       className="py-6 items-center flex justify-between px-[60px] focus:outline-none"
     >
       <div className="flex border gap-8">
-        <Image src={TextLogo} alt="로고" />
-        <Image className="absolute left-48 top-8" src={Search} alt="검색" />
+        <Image
+          src={TextLogo}
+          alt="로고"
+          className="cursor-pointer"
+          onClick={() => router.push("/")}
+        />
+        <Image
+          className="absolute left-48 top-8 cursor-pointer"
+          src={Search}
+          alt="검색"
+        />
         <input
           placeholder="지스토리에 검색"
           type="text"
